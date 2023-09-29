bb4f408682646733ba4354d2c5fff7463fe230b5
EmmanLoria <97245770+EmmanLoria@users.noreply.github.com>
2023-09-29 12:24:01 -0700

    Added checks for validation in login method in the HomeController. Added blogs.html.

--- Milestone2/src/main/java/com/gcu/controller/HomeController.java
+++ Milestone2/src/main/java/com/gcu/controller/HomeController.java
@@ -2,10 +2,14 @@ package com.gcu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gcu.model.LoginModel;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -18,16 +22,26 @@ public class HomeController {
 		model.addAttribute("message", "Welcome to The Blog Who Cried Wolf!");
 		return "home";
 	}
+	
 	@GetMapping("/login")
-	public String printLogin(Model model) {
-		
+	public String displayLogin(Model model) {
 		model.addAttribute("title", "Login Form");
+		model.addAttribute("message", "Login");
 		LoginModel loginModel = new LoginModel();
 		model.addAttribute(loginModel);
-		
-		//Simply return a Model with an attribute named message and return a View named hello using a passed in ModelMap
-		model.addAttribute("message", "Login");
 		return "login";
+	}
+	
+	@PostMapping("/doLogin")
+	public String printLogin(@Valid LoginModel login, BindingResult bindingResult, Model model) {
+		
+		// Check for validation errors
+				if(bindingResult.hasErrors()) {
+					model.addAttribute("title", "Login Form");
+					return "login";
+				}
+		
+		return "blogs";
 	}
 	@GetMapping("/signUp")
 	public String printSignUp(Model model) {
