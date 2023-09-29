d7fd095cb6884144ab33677ecd71871c87741231
mhampton2003 <mhampton2003@gmail.com>
2023-09-29 10:30:40 -0700

    ayo

--- Milestone2/src/main/java/com/gcu/Milestone2Application.java
+++ Milestone2/src/main/java/com/gcu/Milestone2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Milestone2Application {
 
 	public static void main(String[] args) {
+		System.out.println("ayo");
 		SpringApplication.run(Milestone2Application.class, args);
 	}
 
