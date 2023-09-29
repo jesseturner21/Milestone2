2666c2581e30fe697d24fb6f2b4f6f3e27bbd079
mhampton2003 <mhampton2003@gmail.com>
2023-09-29 10:48:18 -0700

    home controller - maya

---
+++ Milestone2/src/main/java/com/gcu/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.gcu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@GetMapping("/")
+	public String printHello(Model model) {
+		
+		//Simply return a Model with an attribute named message and return a View named hello using a passed in ModelMap
+		model.addAttribute("message", "Welcome to our blog!");
+		return "home";
+	}
+
+}
