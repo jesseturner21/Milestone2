7dfe27780f07bb32baa39f98cfadd122093914a9
jesseturner21 <jesseturner2021@gmail.com>
2023-10-18 11:45:02 -0700

    services fixed w blog model, home screen button, bloghtml rename

--- Milestone2/src/main/java/com/gcu/business/BlogsBusinessServiceInterface.java
+++ Milestone2/src/main/java/com/gcu/business/BlogsBusinessServiceInterface.java
@@ -2,9 +2,9 @@ package com.gcu.business;
 
 import java.util.List;
 
-import com.gcu.model.OrderModel;
+import com.gcu.model.BlogModel;
 
 public interface BlogsBusinessServiceInterface {
 	public void test();
-	public List<BlogModel> getOrders();
+	public List<BlogModel> getBlogs();
 }
